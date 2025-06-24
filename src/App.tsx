4133b44d7dd725de46cec677fa164287a3a41178
Gethray99 <98901059+Gethray99@users.noreply.github.com>
2025-06-24 18:36:25 +0530

    add the weather card with weather api

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from "./Header";
+import WeatherCard from "./WeatherCard";
 
 function App() {
-  
+
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+    console.log("API Key:", apiKey);
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Colombo`)
+      .then(res => res.json())
+      .then(data => {
+        setWeather({
+          icon: `https:${data.current.condition.icon}`,
+          condition: data.current.condition.text,
+          temp: data.current.temp_c,
+          humidity: data.current.humidity,
+          wind: data.current.wind_kph,
+          uv: data.current.uv
+        });
+      });
+  }, []);
 
   return (
     <>
       <Header />
       <div style= {{paddingTop: '10px', padding: '16px'}}>
-        <h1>Hello Gethwan!</h1>
+        {weather && <WeatherCard weather={weather} />}
       </div>
     </>
   )
