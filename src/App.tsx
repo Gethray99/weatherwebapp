6e9c48e1cc24e20d235cb0676d44cfc52d880d6f
Gethray99 <98901059+Gethray99@users.noreply.github.com>
2025-06-26 11:26:29 +0530

    Added loading states and error handling

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,27 @@ import './App.css';
 import Header from "./Header";
 import WeatherCard from "./WeatherCard";
 import MapView from "./MapView";
+import { ClipLoader } from 'react-spinners';
 
 function App() {
   const [weather, setWeather] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
   const [location, setLocation] = useState({ lat: 6.9271, lng: 79.8612 });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+    setLoading(true);
+    setError(null);
     fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${searchTerm || "Colombo"}`)
-      .then(res => res.json())
+      .then(res => {
+        if (!res.ok){
+          throw new Error('Network response was not ok');
+          setLoading(false);
+        }
+        return res.json();
+      })
       .then(data => {
         setWeather({
           icon: `https:${data.current.condition.icon}`,
@@ -23,6 +34,12 @@ function App() {
           wind: data.current.wind_kph,
           uv: data.current.uv
         });
+        setLoading(false);
+      })
+      .catch(err => {
+        console.error("Error fetching weather data:", err);
+        setError("Failed to fetch weather data. Please Check your location and Try again.");
+        setLoading(false);
       });
   }, [location]);
 
@@ -47,7 +64,9 @@ function App() {
       <div style={{ position: 'relative' }}>
         <MapView location={location} />
         <div style={{ position: 'absolute', top: 100, left: 50 }}>
-          {weather && <WeatherCard weather={weather} />}
+          {loading && <ClipLoader color="#ffffff" size={50} loading={loading} />}
+          {error && <div className="error">{error}</div>}
+          {weather && !loading && !error && <WeatherCard weather={weather} />}
         </div>
       </div>
     </>
